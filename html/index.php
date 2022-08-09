fbc4eb3a50817ba665280223e525e76c56ac396e
Nori-13 <roomofmelacholy.13@gmail.com>
2022-08-09 10:59:34 +0000

    課題箇所変更

--- html/index.php
+++ html/index.php
@@ -20,6 +20,11 @@ function newtweet($tweet_textarea)
     // 汎用ログインチェック処理をルータに作る。早期リターンで
     createTweet($tweet_textarea, $_SESSION['user_id']);
 }
+
+function newReplyTweet($tweet_textarea,$reply_post_id)
+{
+    createReTweet($tweet_textarea, $reply_post_id, $_SESSION['user_id']);
+}
 /**
  * ログアウト処理を行う。
  */
@@ -29,19 +34,43 @@ function logout()
     $msg = 'ログアウトしました。';
 }
 
-if ($_POST) { /* POST Requests */
+if ($_POST) { /* POST Requests */ 
+      //var_dump($_POST);
     if (isset($_POST['logout'])) { //ログアウト処理
         logout();
         header("Location: login.php");
     } else if (isset($_POST['tweet_textarea'])) { //投稿処理
-        newtweet($_POST['tweet_textarea']);
-        header("Location: index.php");
+        if (isset($_POST['reply_post_id'])) {
+	      //reply_post_idがある場合の処理= 返信
+	          newReplyTweet($_POST['tweet_textarea'], $_POST['reply_post_id']);
+        } else {
+          //新規
+            newtweet($_POST['tweet_textarea']);
+            header("Location: index.php");
+        }
     }
 }
 
 $tweets = getTweets();
 $tweet_count = count($tweets);
 /* 返信課題はここからのコードを修正しましょう。 */
+function getUserName($post_id) {
+  $sql = 'select u.name ';
+    $sql .= ' from tweets t join users u on t.user_id = u.id';
+    $sql .= ' where t.id =' . "$post_id";
+    //var_dump($sql);
+    $stmt = getPdo()->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    //var_dump($result['name']);
+    $name = $result['name'];/* $post_idを元にユーザー名を取得する処理を記載しましょう。 */
+    return $name;
+}
+
+function getUserReplyText($post_id) {
+    //「Re: @名前」の文字列を作りましょう。
+    return "Re: @" . getUserName($post_id) . ' ';
+}
 /* 返信課題はここからのコードを修正しましょう。 */
 ?>
 
@@ -51,13 +80,20 @@ $tweet_count = count($tweets);
 <?php require_once('head.php'); ?>
 
 <body>
+<?php //var_dump($_GET); ?>
   <div class="container">
     <h1 class="my-5">新規投稿</h1>
     <div class="card mb-3">
       <div class="card-body">
         <form method="POST">
-          <textarea class="form-control" type=textarea name="tweet_textarea" ?><!-- 返信課題はここを修正しましょう。 --></textarea>
+          <textarea class="form-control" type=textarea name="tweet_textarea" ><?php if (isset($_GET['reply'])) { 
+	            //リクエストパラメータにreplyがある場合の処理
+	            echo getUserReplyText($_GET['reply']);
+            } ?></textarea>
           <!-- 返信課題はここからのコードを修正しましょう。 -->
+          <?php if (isset($_GET['reply'])) { ?>
+          <input type="hidden" name="reply_post_id" value="<?= "{$_GET['reply']}" ?>"/>
+          <?php } ?>
           <!-- 返信課題はここからのコードを修正しましょう。 -->
           <br>
           <input class="btn btn-primary" type=submit value="投稿">
@@ -67,14 +103,14 @@ $tweet_count = count($tweets);
     <h1 class="my-5">コメント一覧</h1>
     <?php foreach ($tweets as $t) { ?>
       <div class="card mb-3">
-       <div class="card-body">
+        <div class="card-body">
           <p class="card-title"><b><?= "{$t['id']}" ?></b> <?= "{$t['name']}" ?> <small><?= "{$t['updated_at']}" ?></small></p>
           <p class="card-text"><?= "{$t['text']}" ?></p>
           <!--返信課題はここから修正しましょう。-->
           <?php if (isset($t['reply_id'])) { ?>
-          <p>[返信する] [返信元のメッセージ]</p>
-          }else{
-          <p><a href="/view.php?id=<?= "{$t['reply_id']}" ?>">[返信元のメッセージ]</a></p>
+          <a href="index.php?reply=<?= "{$t['id']}" ?>">[返信する]</a> <a href="/view.php?id=<?= "{$t['reply_id']}" ?>">[返信元のメッセージ]</a>
+          <?php }else{ ?>
+          <p><a href="index.php?reply=<?= "{$t['id']}" ?>">[返信する]</a></p>
           <?php } ?>
           <!--返信課題はここまで修正しましょう。-->
         </div>
--- html/view.php
+++ html/view.php
@@ -3,7 +3,6 @@ require_once('db.php');
 require_once('functions.php');
 
 /* 返信課題はここからのコードを修正しましょう。 */
-require_once('db.php');
 $t = getTweet($_GET['id']);
 /* 返信課題はここまでのコードを修正しましょう。 */
 ?>
@@ -22,9 +21,8 @@ $t = getTweet($_GET['id']);
     <div class="card mb-3">
       <div class="card-body">
         <!-- 返信課題はここからのコードを修正しましょう。 -->
-          <p class="card-title"><b><?= "{$t['id']}" ?></b> <?= "{$t['name']}" ?> <small><?= "{$t['updated_at']}" ?></small></p>
+          <p class="card-title"><b><?= "{$t['id']}" ?></b> <small><?= "{$t['name']}" ?> <?= "{$t['updated_at']}" ?></small></p>
           <p class="card-text"><?= "{$t['text']}" ?></p>
-          <p>[返信する] [返信元のメッセージ]</p>
         <!-- 返信課題はここまでのコードを修正しましょう。 -->
       </div>
     </div>
