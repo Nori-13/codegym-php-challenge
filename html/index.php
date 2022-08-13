7b42d796a447076d4bf862edea5fd9f4504946ea
Nori-13 <100354960+Nori-13@users.noreply.github.com>
2022-08-13 12:42:44 +0900

    Update index.php
    index.php　インデントを揃える

--- html/index.php
+++ html/index.php
@@ -36,14 +36,14 @@ function logout()
 
 if ($_POST) { /* POST Requests */
     if (isset($_POST['logout'])) { //ログアウト処理
-    　　logout();
-        header("Location: login.php");
+	      logout();
+              header("Location: login.php");
     } else if (isset($_POST['tweet_textarea'])) { //投稿処理
-        if (isset($_POST['reply_post_id'])) {
-	          newReplyTweet($_POST['tweet_textarea'], $_POST['reply_post_id']);
+    	if (isset($_POST['reply_post_id'])) {
+	      newReplyTweet($_POST['tweet_textarea'], $_POST['reply_post_id']);
         } else {
-            newtweet($_POST['tweet_textarea']);
-            header("Location: index.php");
+              newtweet($_POST['tweet_textarea']);
+              header("Location: index.php");
         }
     }
 }
