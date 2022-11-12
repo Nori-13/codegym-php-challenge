826179e3b087110c227c5ecd8e170896effc574d
Nori-13 <roomofmelacholy.13@gmail.com>
2022-11-12 05:16:08 +0000

    sqlファイル制作、修正

--- html/images/sql/favorite.sql
+++ html/images/sql/favorite.sql
@@ -1,8 +1,8 @@
 CREATE TABLE favorites(
-    id int AUTO_INCREMENT,
+ id int AUTO_INCREMENT,
     member_id int,
     post_id int,
-    created_at DEFAULT CURRENT_TIMESTAMP,
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP,
     updated_at timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
 );
